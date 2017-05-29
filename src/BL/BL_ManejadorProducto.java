cd02cb40706ee02769220f90be8d5320c2cb314a
oscal <ocubero10>
2017-05-29 10:42:24 -0600

    Clase ManejadorProducto creada

---
+++ src/BL/BL_ManejadorProducto.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author oscal
+ */
+public class BL_ManejadorProducto {
+    private ArrayList<BL_Producto> listaProducto;
+    
+    
+    public ArrayList<BL_Producto> ObtenerListaProductos(){
+        return listaProducto;
+    }
+    
+    public void Agregar(BL_Producto producto){
+        listaProducto.add(producto);
+    }
+    
+    public void Eliminar(BL_Producto producto){
+        listaProducto.remove(producto);
+    }
+    
+    public void CargarProductos(){
+        //cargarLista
+        
+    }
+    
+    public void BuscarPorFiltro(){
+        
+    }
+    
+    public void metodoPrueba(){}
+}
