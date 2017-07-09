77002428ac8c51179b7af48d3fcddbce6fb645b9
jostm13 <jostm13@outlook.com>
2017-07-09 11:49:34 -0600

    Merge origin/master into Josette2
    Conflicts:
    	src/UI/DetalleProducto.java

--- src/UI/AdministrarUsuarios.java
+++ src/UI/AdministrarUsuarios.java
@@ -34,6 +34,7 @@ public class AdministrarUsuarios extends javax.swing.JFrame {
     
     public AdministrarUsuarios() {
         initComponents();
+        
         modelo = (DefaultTableModel) tablaUsuarios.getModel();
         tablaUsuarios.getTableHeader().setReorderingAllowed(false);
         tablaUsuarios.getTableHeader().setDefaultRenderer(new Facturar.HeaderColor());
